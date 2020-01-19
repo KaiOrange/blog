9a4302c9d5bdfa8a3a28f323d4478d7c0a1480c1
Orange <huangkaiking@126.com>
2020-01-19 16:59:00 +0800

    新增友链朋友

--- themes/next/source/js/src/customization.js
+++ themes/next/source/js/src/customization.js
@@ -7,11 +7,16 @@ $(document).ready(function () {
     // $("body").append('<script type="text/javascript" src="https://cdn.bootcss.com/canvas-nest.js/2.0.4/canvas-nest.js" ></script>');
     $("body").append('<script type="text/javascript" src="https://cdn.bootcss.com/canvas-nest.js/1.0.0/canvas-nest.min.js"></script>');
   } else if ($env.isAndroid) {
-    var url = 'hap://app/top.kai666666.blog';
-    if (location.pathname && location.pathname !== '/') {
-      url = 'hap://app/top.kai666666.blog/Webview?url='+ encodeURIComponent(decodeURIComponent(location.href));
+    var isFirstBrowse = localStorage.getItem('isFirstBrowse') !== 'false';
+    // 非第一次浏览有30%的概率试图去唤起快应用
+    if (isFirstBrowse || Math.floor(Math.random() * 10) < 4) {
+      var url = 'hap://app/top.kai666666.blog';
+      if (location.pathname && location.pathname !== '/') {
+        url = 'hap://app/top.kai666666.blog/Webview?url='+ encodeURIComponent(decodeURIComponent(location.href));
+      }
+      localStorage.setItem('isFirstBrowse','false');
+      location.href = url;
     }
-    location.href = url;
   }
 });
 
